e3c8a605b1a77b2bce4efe9945e410d0c4cc945f
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-07-03 15:54:28 -0700

    new edit

--- components/Home/PremiumAdsComponent.tsx
+++ components/Home/PremiumAdsComponent.tsx
@@ -8,10 +8,7 @@ export default function PremiumAdsComponent() {
         <h2 className="text-3xl">Премиум Объявления</h2>
       </div>
       <div className="container grid max-md:grid-cols-2 grid-cols-4 grid-flow-row gap-4">
-        <PremiumCard />
-        <PremiumCard />
-        <PremiumCard />
-        <PremiumCard />
+         {/* <PremiumCard /> */}
       </div>
     </section>
   );
